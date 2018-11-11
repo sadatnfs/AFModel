75c7064f5f2b072ff53dae97f4684013f792fe57
Nafis Sadat <sadatnfs@uw.edu>
2018-11-11 13:48:50 -0800

    Add a location subsetting option in get_oots

--- AFModel/R/AFModel_helpers.r
+++ AFModel/R/AFModel_helpers.r
@@ -93,18 +93,29 @@ get_model_info <- function(entity_id = NA) {
 
 
 ## Function to query and get output info for given entity and model
-get_oots <- function(entity_id, model_id, scenario = 0, year_id = c(1990:2040)) {
+get_oots <- function(entity_id, model_id, scenario = 0, year_id = c(1980:2050), location_id = -1) {
   dbconn <- dbConnect(MySQL(),
     user = "readonly", password = "justlooking",
     dbname = "forecasting", host = "dex-modelingdb-d01.ihme.washington.edu"
   )
 
-  out <- dbSendQuery(dbconn, paste0(
-    'select * from outputs where entity = "', entity_id, '"',
-    " and year_id IN (", paste0(year_id, collapse = ","), ")",
-    " and model_id = ", model_id,
-    " and scenario IN (", paste0(scenario, collapse = ","), ")"
-  ))
+  if(location_id == -1)  {
+    out <- dbSendQuery(dbconn, paste0(
+      'select * from outputs where entity = "', entity_id, '"',
+      " and model_id = ", model_id,
+      " and year_id IN (", paste0(year_id, collapse = ","), ")",
+      " and scenario IN (", paste0(scenario, collapse = ","), ")"
+    ))
+  } else {
+    out <- dbSendQuery(dbconn, paste0(
+      'select * from outputs where entity = "', entity_id, '"',
+      " and model_id = ", model_id,
+      " and location_id IN (", paste0(location_id, collapse = ","), ")",
+      " and year_id IN (", paste0(year_id, collapse = ","), ")",
+      " and scenario IN (", paste0(scenario, collapse = ","), ")"
+    ))
+  }
+
   out_2 <- data.table(fetch(out, n = -1))
   on.exit(dbDisconnect(dbconn))
 
