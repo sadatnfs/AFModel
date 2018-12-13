a1d65af22e4ef65114e42bf606735e1b753869c2
Nafis Sadat <sadatnfs@uw.edu>
2018-12-13 09:37:18 -0800

    V1.0.9 Add better documentation of stat_maker for others to use

--- AFModel/R/stat_maker.R
+++ AFModel/R/stat_maker.R
@@ -1,17 +1,21 @@
 ### Stat making function across draws
-#' @title FUNCTION_TITLE
-#' @description FUNCTION_DESCRIPTION
-#' @param data PARAM_DESCRIPTION
-#' @param melt PARAM_DESCRIPTION, Default: F
-#' @param merge PARAM_DESCRIPTION, Default: T
-#' @param idvar PARAM_DESCRIPTION, Default: c("iso3", "year")
-#' @return OUTPUT_DESCRIPTION
-#' @details DETAILS
+#' @title Stat maker
+#' @description Create row-wise uncertainty intervals from a set of simulated columns of data
+#' @param data a \code{data.table} with draws
+#' @param melt melt the data long if the draws are wide , Default: F
+#' @param merge merge the statistics with the draws dataframe?, Default: T
+#' @param idvar unique identifier, Default: c("iso3", "year")
+#' @return a dataset with the unique identifier, mean and 2.5th and 97.5th quantiles, and optionally, the set of draws merged on if \code{merge} is \code{TRUE}
+#' @details If \code{data} has already been melted long, then the column with the data and draw names \code{MUST} be respectively named \code{data} and \code{draws}
 #' @examples
 #' \dontrun{
-#' if(interactive()){
-#'  #EXAMPLE1
-#'  }
+#'  # Simulate data IDs
+#' sim_data <- data.table(expand.grid(a = c(1:10), b = LETTERS[1:5]))
+#' # Simulate data columns
+#' n_cols <- 10
+#' sim_data[, paste0('draw_', c(1:n_cols)):= lapply(paste0('draw_', c(1:n_cols)), function(x) rnorm(.N, 1, 1))]
+#' # Use stat maker
+#' sim_stats <- stat_maker(data = sim_data, melt = T, merge = F, idvar = c('a', 'b'))
 #' }
 #' @rdname stat_maker
 #' @export
